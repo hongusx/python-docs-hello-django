3601574c6448eb6f46c4fe96630fcac7d23a667f
hongusx <147713014+hongusx@users.noreply.github.com>
2023-10-12 21:56:36 +1100

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello world, this is my first python program running on Azure cloud!")
